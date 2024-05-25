d0a22894d633ed47f37cedede375c0032e12a251
ruchit9510 <ruchitkapuriya9510@gmail.com>
2024-05-25 14:11:49 +0530

    Create pattern13.cpp

---
+++ Patterns/pattern13.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int num1 = 10;
+    int num2 = 20;
+    int sum = num1 + num2;
+
+    std::cout << "The sum of " << num1 << " and " << num2 << " is: " << sum << std::endl;
+
+    return 0;
+}
