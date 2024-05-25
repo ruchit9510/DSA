96f15de34627655f279bc68f4882f7180e2840b6
ruchit9510 <ruchitkapuriya9510@gmail.com>
2024-05-25 12:05:18 +0530

    feat: Add code for printing a square pattern with user-defined number of stars

--- Patterns/pattern1.cpp
+++ Patterns/pattern1.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 using namespace std;
 
-
-int main() {
+void print1(int n){
     int i,j;
-
-    for(i=0;i<4;i++){
-        for(j=0;j<4;j++){
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
             cout<<"* ";
         }
         cout<<endl;
     }
-    
+}
+int main() {
+    int n;
+    cout<<"Enter the number of stars in line : ";
+    cin>>n;
+    print1(n);
     return 0;
 }
