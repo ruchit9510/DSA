161c294557d0795e1e411fccdb6189e3b166c0d0
ruchit9510 <ruchitkapuriya9510@gmail.com>
2024-05-25 13:58:20 +0530

    feat: Add code for printing a triangle pattern with user-defined number of stars

---
+++ Patterns/pattern2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void print1(int n){
+    int i,j;
+    for(i=0;i<n;i++){
+        for(j=0;j<=i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
+int main() {
+    int n,t;
+    cout<<"Enter how many time you want to print : ";
+    cin>>t;
+    for(int i=0 ; i<t ;i++){
+        cout<<"Enter the number of stars in line : ";
+        cin>>n;
+    print1(n);
+    }
+    cin>>n;
+    print1(n);
+    return 0;
+}
