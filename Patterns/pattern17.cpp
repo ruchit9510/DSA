0ca4d1044f7fc8d9b322193123f299eb63634811
ruchit9510 <ruchitkapuriya9510@gmail.com>
2024-05-28 13:50:12 +0530

    feat: Add code for printing triangle and square patterns with user-defined number of stars

---
+++ Patterns/pattern17.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void print1(int n){
+    int i,j;
+    for(i=0;i<n;i++){
+        //space
+        for(j=0;j<n-i-1;j++){ 
+        cout<<" ";
+        }
+        //character
+        char ch='A';
+        int breakpoint = (2*i+1)/2;
+        for(j=1;j<=2*i+1;j++){
+            cout<<ch;
+            if(j<=breakpoint)ch++;
+            else ch--;
+        }
+        
+        //space
+        for(j=0;j<n-i-1;j++){ 
+        cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main() {
+    int n,t;
+    cout<<"Enter how many time you want to print : ";
+    cin>>t;
+    for(int i=0 ; i<t ;i++){
+        cout<<"Enter the number of stars in line : ";
+        cin>>n;
+    print1(n);
+    }
+    return 0;
+}
---
+++ Patterns/pattern18.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+void print1(int n){
+    int i;
+
+    for(i=0;i<n;i++){
+        
+        for(char ch='E'-i;ch<='E';ch++){
+            cout<<ch<<" ";
+        }
+            
+            cout<<endl;
+        }
+    }
+
+int main() {
+    int n,t;
+    cout<<"Enter how many time you want to print : ";
+    cin>>t;
+    for(int i=0 ; i<t ;i++){
+        cout<<"Enter the number of stars in line : ";
+        cin>>n;
+    print1(n);
+    }
+    return 0;
+}
