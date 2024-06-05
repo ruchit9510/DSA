40320ce90f9133ef54a27aa78674cbc635e2ac58
ruchit9510 <ruchitkapuriya9510@gmail.com>
2024-06-05 11:33:50 +0530

    Sum of numbers using parameterised and functional way

--- Recursion/recursion4(sum of N numbers).cpp
+++ Recursion/recursion4(sum of N numbers).cpp
@@ -3,10 +3,16 @@ using namespace std;
 
 void print(int i, int sum){ //Parameterised way
     if(i<1){
-        cout<<"Sum is : "<<sum;
+        cout<<"Sum is : "<<sum<<" (Parameterised way)";
         return ;
     }
     print(i-1,sum+i);
+}
+int print1(int sum){ //Functional way
+    if(sum == 0){
+        return 0;
+    }
+    return sum + print1(sum-1);
 }
 
 int main(){
@@ -14,5 +20,8 @@ int main(){
     cout<<"Enter a NUMBER : ";
     cin>>n;
     print(n,0);
+    cout<<endl;
+    cout<<"Sum is : "<<print1(n)<<" (Functional way)";
+
     return 0;
 }
