ebb013caa8283ec52eb1ecb05c620d05f9f02fe8
ruchit9510 <ruchitkapuriya9510@gmail.com>
2024-06-13 17:38:15 +0530

    Bubble Sort

---
+++ Sorting/Bubble_Sort.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubble_sort(int arr[] , int n){
+    for(int i=n-1; i>=0; i--){
+        for(int j=0; j<=i-1; j++){
+            if(arr[j]>arr[j+1]){
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j]   = temp;
+            }
+        }
+    }
+}
+
+int main(){
+    int n ;
+    cout<<"Enter Size of an Array : ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter Your Array : ";
+    for (int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    bubble_sort(arr , n);
+    cout <<"Sorted Array : ";
+    for (int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
