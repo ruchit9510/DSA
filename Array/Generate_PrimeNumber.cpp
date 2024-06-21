dbc52e4503a3923389fed406965a8a4b8bbbd1f0
ruchit9510 <ruchitkapuriya9510@gmail.com>
2024-06-21 23:11:48 +0530

    .

--- Array/Generate_PrimeNumber.cpp
+++ Array/Generate_PrimeNumber.cpp
@@ -5,7 +5,7 @@ bool isPrime(int num) {
     if (num <= 1) {
         return false;
     }
-    for (int i = 2; i <= sqrt(num); ++i) {
+    for (int i = 2; i <= num/2; ++i) {
         if (num % i == 0) {
             return false;
         }
